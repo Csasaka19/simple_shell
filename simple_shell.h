6c7e73370a2f0af7d4ae5eeda8039c6709145880
Clive_Sasaka <clivesasaka@gmail.com>
2022-05-16 06:50:48 -0700

    Shell

---
+++ simple_shell.h
@@ -0,0 +1,16 @@
+
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <limits.h>
+
+#endif
