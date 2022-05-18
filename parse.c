1b01d2cdb835821452e7d39a565367656e654d48
billowbashir <billowbashir@gmail.com>
2022-05-18 23:44:25 +0300

    fix

--- parse.c
+++ parse.c
@@ -1,23 +1,50 @@
-#include "simple_shell.h"
+#include "hsh.h"
 
-#define DELIMITER ":"
 /**
- * parse_line - parses arguments to a valid command
- * @data: a pointer to the struct of data
+ * parse - parse input
+ * @info: shell information
  *
- * Return: 0 on success, -1 on error
+ * Description: This function expands aliases, variables, and word splitting
+ *
+ * Return: the final number of tokens
  */
-int parse_line(sh_t *data)
+int parse(info_t *info)
 {
-if (is_path_form(data) > 0)
-return (SUCCESS);
-if (is_builtin(data) > 0)
-{
-if (handle_builtin(data) < 0)
-return (FAIL);
-return (NEUTRAL);
+	char **tokens, *tok;
+	size_t n = 0;
+	cmdlist_t *cmd = info->commands = cmd_to_list(info->line);
+
+	while (cmd)
+	{
+		remove_comments(cmd);
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		expand_aliases(info->aliases, &(cmd->tokens));
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		expand_vars(info, &(cmd->tokens));
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		tokens = cmd->tokens;
+		for (tok = *tokens; tok; tok = *(++tokens))
+		{
+			*tokens = dequote(tok);
+			free(tok);
+		}
+		cmd = cmd->next;
+		++n;
+	}
+	return (n);
 }
-is_short_form(data);
-return (SUCCESS);
-}
-#undef  DELIMITER
