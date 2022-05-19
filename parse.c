a04b4528fe35aa504ac5be55d726bc83ff11bb95
Clive_Sasaka <clivesasaka@gmail.com>
2022-05-19 03:07:35 -0700

    SHELL

--- parse.c
+++ parse.c
@@ -23,42 +23,42 @@ return (0);
 =======
 int parse(info_t *info)
 {
-	char **tokens, *tok;
-	size_t n = 0;
-	cmdlist_t *cmd = info->commands = cmd_to_list(info->line);
+char **tokens, *tok;
+size_t n = 0;
+cmdlist_t *cmd = info->commands = cmd_to_list(info->line);
 
-	while (cmd)
-	{
-		remove_comments(cmd);
-		if (!cmd->tokens)
-		{
-			cmd = cmd->next;
-			del_cmd(&(info->commands), n);
-			continue;
-		}
-		expand_aliases(info->aliases, &(cmd->tokens));
-		if (!cmd->tokens)
-		{
-			cmd = cmd->next;
-			del_cmd(&(info->commands), n);
-			continue;
-		}
-		expand_vars(info, &(cmd->tokens));
-		if (!cmd->tokens)
-		{
-			cmd = cmd->next;
-			del_cmd(&(info->commands), n);
-			continue;
-		}
-		tokens = cmd->tokens;
-		for (tok = *tokens; tok; tok = *(++tokens))
-		{
-			*tokens = dequote(tok);
-			free(tok);
-		}
-		cmd = cmd->next;
-		++n;
-	}
-	return (n);
+while (cmd)
+{
+remove_comments(cmd);
+if (!cmd->tokens)
+{
+cmd = cmd->next;
+del_cmd(&(info->commands), n);
+continue;
+}
+expand_aliases(info->aliases, &(cmd->tokens));
+if (!cmd->tokens)
+{
+cmd = cmd->next;
+del_cmd(&(info->commands), n);
+continue;
+}
+expand_vars(info, &(cmd->tokens));
+if (!cmd->tokens)
+{
+cmd = cmd->next;
+del_cmd(&(info->commands), n);
+continue;
+}
+tokens = cmd->tokens;
+for (tok = *tokens; tok; tok = *(++tokens))
+{
+*tokens = dequote(tok);
+free(tok);
+}
+cmd = cmd->next;
+++n;
+}
+return (n);
 >>>>>>> 1b01d2cdb835821452e7d39a565367656e654d48
 }
